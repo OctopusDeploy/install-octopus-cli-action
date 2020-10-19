6d224a7c4aea7ab3ccdfb985b3f0cf55f01e75b8
Michael Levan <mlevan1992@gmail.com>
2020-10-19 13:47:37 -0400

    Install Octopus CLI

---
+++ installoctocli.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	// "os"
+	"os/exec"
+	"runtime"
+)
+
+func main() {
+	if runtime.GOOS == "windows" {
+		install := exec.Command("choco", "install", "octopustools", "-y")
+		err := install.Run()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if runtime.GOOS == "linux" {
+		install := exec.Command("apt", "update", "&&", "apt", "install", "--no-install-recommends", "gnupg", "curl", "ca-certificates", "apt-transport-https", "&&", "curl", "-sSfL", "https://apt.octopus.com/public.key", "|", "apt-key", "add", "-", "&&", "sh", "-c", "echo", "deb", "https://apt.octopus.com/", "stable", "main", ">", "/etc/apt/sources.list.d/octopus.com.list", "&&", "apt", "update", "&&", "apt", "install", "octopuscli")
+		err := install.Run()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if runtime.GOOS == "darwin" {
+		cmd := exec.Command("brew", "install", "octopuscli", "-f")
+		checkErr := cmd.Run()
+
+		if checkErr != nil {
+			fmt.Println("Confirm that octopuscli is not already installed...")
+			fmt.Println(checkErr)
+		}
+	}
+}
