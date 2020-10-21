cd7e87096d5cf9e710524ebe8127488ba348772d
Michael Levan <mlevan1992@gmail.com>
2020-10-21 13:27:18 -0400

    push cmd

--- installoctocli.go
+++ installoctocli.go
@@ -20,13 +20,11 @@ func main() {
 	}
 
 	if runtime.GOOS == "linux" {
-		// install := exec.Command("sudo", "apt update && sudo apt install --no-install-recommends gnupg curl ca-certificates apt-transport-https && curl -sSfL https://apt.octopus.com/public.key | sudo apt-key add - && sudo sh -c 'echo deb https://apt.octopus.com/ stable main > /etc/apt/sources.list.d/octopus.com.list'")
-		// install := exec.Command("sudo", "apt", "install", "--no-install-recommends", "gnupg", "curl", "ca-certificates apt-transport-https", "&&", "curl", "sSfL", "https://apt.octopus.com/public.key", "|", "sudo", "apt-key", "add", "-", "&&", "sudo sh -c 'echo deb https://apt.octopus.com/ stable main > /etc/apt/sources.list.d/octopus.com.list'", "&&", "sudo", "apt", "update", "&&", "sudo", "apt", "install", "octopuscli")
 		cmd1 := exec.Command("sudo", "apt", "install", "--no-install-recommends", "gnupg", "curl", "ca-certificates", "apt-transport-https")
-		// cmd2 := exec.Command("curl", "-sSfL", "https://apt.octopus.com/public.key", "|", "sudo", "apt-key add")
-		cmd2 := exec.Command("sudo", "sh", "-c", "echo", "'deb https://apt.octopus.com/ stable main > /etc/apt/sources.list.d/octopus.com.list'")
-		cmd3 := exec.Command("sudo", "apt", "update")
-		cmd4 := exec.Command("sudo", "apt", "install", "octopuscli")
+		cmd2 := exec.Command("curl", "-sSfL", "https://apt.octopus.com/public.key", "|", "apt-key add")
+		cmd3 := exec.Command("sudo", "sh", "-c", "echo", "'deb https://apt.octopus.com/ stable main > /etc/apt/sources.list.d/octopus.com.list'")
+		cmd4 := exec.Command("sudo", "apt", "update")
+		cmd5 := exec.Command("sudo", "apt", "install", "octopuscli")
 
 		cmd1.Stdout = os.Stdout
 		cmd1.Stderr = os.Stderr
@@ -40,10 +38,14 @@ func main() {
 		cmd4.Stdout = os.Stdout
 		cmd4.Stderr = os.Stderr
 
+		cmd5.Stdout = os.Stdout
+		cmd5.Stderr = os.Stderr
+
 		err1 := cmd1.Run()
 		err2 := cmd2.Run()
 		err3 := cmd3.Run()
 		err4 := cmd4.Run()
+		err5 := cmd5.Run()
 
 		if err1 != nil {
 			log.Fatal(err1)
@@ -59,6 +61,10 @@ func main() {
 
 		if err4 != nil {
 			log.Fatal(err4)
+		}
+
+		if err5 != nil {
+			log.Fatal(err5)
 		}
 	}
 
