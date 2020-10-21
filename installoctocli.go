bf82afc647c71fcb08d0974928ae442a90688caa
Michael Levan <mlevan1992@gmail.com>
2020-10-21 13:21:14 -0400

    cmd update

--- installoctocli.go
+++ installoctocli.go
@@ -24,15 +24,15 @@ func main() {
 		// install := exec.Command("sudo", "apt", "install", "--no-install-recommends", "gnupg", "curl", "ca-certificates apt-transport-https", "&&", "curl", "sSfL", "https://apt.octopus.com/public.key", "|", "sudo", "apt-key", "add", "-", "&&", "sudo sh -c 'echo deb https://apt.octopus.com/ stable main > /etc/apt/sources.list.d/octopus.com.list'", "&&", "sudo", "apt", "update", "&&", "sudo", "apt", "install", "octopuscli")
 		cmd1 := exec.Command("sudo", "apt", "install", "--no-install-recommends", "gnupg", "curl", "ca-certificates", "apt-transport-https")
 		// cmd2 := exec.Command("curl", "-sSfL", "https://apt.octopus.com/public.key", "|", "sudo", "apt-key add")
-		cmd3 := exec.Command("sudo", "sh", "-c", "echo", "'deb https://apt.octopus.com/ stable main > /etc/apt/sources.list.d/octopus.com.list'")
-		// cmd4 := exec.Command("sudo", "apt", "update")
+		cmd2 := exec.Command("sudo", "sh", "-c", "echo", "'deb https://apt.octopus.com/ stable main > /etc/apt/sources.list.d/octopus.com.list'")
+		cmd3 := exec.Command("sudo", "apt", "update")
 		cmd4 := exec.Command("sudo", "apt", "install", "octopuscli")
 
 		cmd1.Stdout = os.Stdout
 		cmd1.Stderr = os.Stderr
 
-		// cmd2.Stdout = os.Stdout
-		// cmd2.Stderr = os.Stderr
+		cmd2.Stdout = os.Stdout
+		cmd2.Stderr = os.Stderr
 
 		cmd3.Stdout = os.Stdout
 		cmd3.Stderr = os.Stderr
@@ -41,7 +41,7 @@ func main() {
 		cmd4.Stderr = os.Stderr
 
 		err1 := cmd1.Run()
-		// err2 := cmd2.Run()
+		err2 := cmd2.Run()
 		err3 := cmd3.Run()
 		err4 := cmd4.Run()
 
@@ -49,9 +49,9 @@ func main() {
 			log.Fatal(err1)
 		}
 
-		// if err2 != nil {
-		//	log.Fatal(err2)
-		// }
+		if err2 != nil {
+			log.Fatal(err2)
+		}
 
 		if err3 != nil {
 			log.Fatal(err3)
