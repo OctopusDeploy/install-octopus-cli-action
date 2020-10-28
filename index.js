3d6cd760937905e7961a7287aebe5d7739002911
Michael Levan <mlevan1992@gmail.com>
2020-10-28 11:56:41 -0400

    Updated index to call installoctocli function

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const core = require('@actions/core')
 const octocli = require('./lib/installoctocli')
 
 async function octopuscli() {
+    const version = core.getInput('version')
+
     try {
-        await setup()
-    } catch { 
+        await installoctocli(version)
+    } catch (e){ 
         core.setFailed(error.message)
     }
 }
