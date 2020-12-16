d7d1a3ec9826988ac08f3e95d03332b9725a22fa
Steve Winton <stevewinton@gmail.com>
2020-12-16 16:40:56 -0600

    Call the octocli function
    This fixes the error that is being thrown: "installoctocli is not defined"

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function octopuscli() {
     const version = core.getInput('version')
 
     try {
-        await installoctocli(version)
+        await octocli(version)
     } catch (e){ 
         core.setFailed(error.message)
     }
