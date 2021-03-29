e022062ddf8123274cfb6b4bd7826d1749d52056
jbristowe <john.bristowe@octopus.com>
2021-03-29 16:02:38 +1000

    chore: updated distribution

---
+++ dist/test-setup.js
@@ -0,0 +1,30 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const tmp = __importStar(require("tmp"));
+tmp.setGracefulCleanup();
+const tmpdir = tmp.dirSync({ template: 'octopus-cli-XXXXXX' });
+process.env = Object.assign(process.env, {
+    RUNNER_TEMP: tmpdir.name,
+    RUNNER_TOOL_CACHE: tmpdir.name,
+    GITHUB_ACTION: '1'
+});
+//# sourceMappingURL=test-setup.js.map
