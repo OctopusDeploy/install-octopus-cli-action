8a9cfdc12c37111ff145fba6a9d34b08b3c1cf4a
John Bristowe <john.bristowe@octopus.com>
2022-05-03 22:48:51 +1000

    tests: added for wildcard support

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -9,8 +9,15 @@ describe('installer', () => {
     })
   }, 100000)
 
-  test('acquires a version of Octopus CLI', () => {
-    return installOctopusCli('7.4.3124').then(data => {
+  test('fails to acquire a version of Octopus CLI', () => {
+    expect.assertions(1)
+    return installOctopusCli('0.0.0').catch(e => {
+      expect(<Error>e.message).toContain('Octopus CLI version not found')
+    })
+  }, 100000)
+
+  test('acquires version 8.* of Octopus CLI', () => {
+    return installOctopusCli('8.*').then(data => {
       expect(data).toContain(path.sep + 'octo' + path.sep)
     })
   }, 100000)
